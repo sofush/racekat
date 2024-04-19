2458e878786ec30b1b7dc00f9b3eebb7e66b9518
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-04-19 20:36:33 +0200

    feat: redirect index to user's profile

--- src/main/java/com/example/racekat/controller/IndexController.java
+++ src/main/java/com/example/racekat/controller/IndexController.java
@@ -3,10 +3,12 @@ package com.example.racekat.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String index(Principal principal) {
+        return "redirect:/user/" + principal.getName();
     }
 }
