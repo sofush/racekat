43e1a107341076673745eb6ff6257461f5d2cec2
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-04-23 20:24:11 +0200

    feat: add unit tests for adding a user

--- src/test/java/com/example/racekat/MainTests.java
+++ src/test/java/com/example/racekat/MainTests.java
@@ -1,11 +1,68 @@
 package com.example.racekat;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.*;
+import com.example.racekat.entity.Role;
+import com.example.racekat.entity.User;
+import com.example.racekat.repository.UserRepository;
+import com.example.racekat.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MainTests {
 	@Test
-	void contextLoads() {
+	public void testAddValidUserSucceeds() {
+		User validUser = new User(
+			"valid-user",
+			"password123",
+			Role.USER,
+			"",
+			"",
+			List.of()
+		);
+
+		UserRepository mockRepo = Mockito.mock(UserRepository.class);
+		UserService service = new UserService(mockRepo);
+
+		Mockito
+			.when(mockRepo.findUserByUsername(validUser.getUsername()))
+			.thenReturn(validUser);
+
+		service.addUser(
+			validUser.getUsername(),
+			validUser.getPassword(),
+			validUser.getName(),
+			validUser.getAbout()
+		);
+
+		User ret = service.findUserByUsername(validUser.getUsername());
+		assertThat(validUser).isEqualTo(ret);
+	}
+
+	@Test
+	public void testAddInvalidUserFails() {
+		User invalidUser = new User(
+			"short-password-user",
+			"pw",
+			Role.USER,
+			"",
+			"",
+			List.of()
+		);
+
+		UserRepository mockRepo = Mockito.mock(UserRepository.class);
+		UserService service = new UserService(mockRepo);
+
+		Exception e = assertThrows(IllegalArgumentException.class, () -> service.addUser(
+            invalidUser.getUsername(),
+            invalidUser.getPassword(),
+            invalidUser.getName(),
+            invalidUser.getAbout()
+        ));
+		assertThat(e.getMessage().toLowerCase()).contains("password");
 	}
 }
