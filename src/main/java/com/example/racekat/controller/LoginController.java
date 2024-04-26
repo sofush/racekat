6d90ac11daefcc6b6d3c59638b84d3a2b232f40d
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-04-26 09:05:31 +0200

    feat: add logout link in /members

--- src/main/java/com/example/racekat/controller/LoginController.java
+++ src/main/java/com/example/racekat/controller/LoginController.java
@@ -1,12 +1,23 @@
 package com.example.racekat.controller;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
     public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        SecurityContext ctx = SecurityContextHolder.getContext();
+        ctx.setAuthentication(null);
         return "login";
     }
 }
